ae2f8888514efc31ee3d03ca4a49bbe58cd4d053
shah9661 <shanshadalam96@gmail.com>
2025-09-01 19:03:42 +0530

    Initial portfolio

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
+@app.route("/skill")
+def skill():
+    return render_template("skill.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
